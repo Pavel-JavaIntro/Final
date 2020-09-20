cc185606ded5e217daba4b8bd282e195d7005783
Pavel Kassitchev <pavkascool@gmail.com>
2020-09-20 16:29:02 +0300

    second push

--- src/main/java/by/pavka/LibraryServlet.java
+++ src/main/java/by/pavka/LibraryServlet.java
@@ -1,15 +1,29 @@
 package by.pavka;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-public class LibraryServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+public class LibraryServlet extends HttpServlet {
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    process(request, response);
+  }
 
-    }
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    process(request, response);
+  }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        PrintWriter printWriter = response.getWriter();
-        printWriter.println("Yes, it is");
-    }
+  private void process(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    RequestDispatcher requestDispatcher = getServletContext().getRequestDispatcher("/success.jsp");
+    requestDispatcher.forward(request, response);
+  }
 }
--- web/index.jsp
+++ web/index.jsp
@@ -1,20 +1,24 @@
-<%--
-  Created by IntelliJ IDEA.
-  User: User
-  Date: 20.09.2020
-  Time: 12:48
-  To change this template use File | Settings | File Templates.
---%>
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
 <html>
-  <head>
-    <title>Library</title>
-  </head>
-  <body>
-  Pavel Library
-  <br/>
-  <form action="library">
-    <input type="submit" value="Execute">
-  </form>
-  </body>
+<head><title>Login</title></head>
+<body>
+<form name="loginForm" method="POST" action="library">
+    <h1>Библиотека Павла Касичева<br/></h1>
+    <br/>
+    <h2>Войдите в свою учетную запись<br/></h2>
+    <br/>
+    <input type="hidden" name="command" value="login"/>
+    Фамилия:<br/>
+    <input type="text" name="surname" value=""/>
+    <br/>Имя:<br/>
+    <input type="text" name="name" value=""/>
+    <br/>Пароль:<br/>
+    <input type="password" name="password" value=""/>
+    <br/> ${errorLoginPassMessage} <br/>
+    ${wrongAction} <br/>
+    ${nullPage} <br/>
+    <input type="submit" value="Войти"/>
+</form>
+<hr/>
+</body>
 </html>
